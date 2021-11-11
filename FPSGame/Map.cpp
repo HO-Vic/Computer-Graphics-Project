178f696052c523da3c5f54b6b106e2780e19c637
ckdrms <hoi000115@gmail.com>
2021-11-11 23:31:57 +0900

    Map.cpp GetMatrix()에 Revolutoin추가 그 외 result를 변경시키는 translate함수나 Revoltoin함수 삭제 ,Map.h Map.cpp둘다 변경

--- FPSGame/Map.cpp
+++ FPSGame/Map.cpp
@@ -2,19 +2,19 @@
 Map::Map() {
 	Position = glm::vec3(0.0f, 0.0f, 0.0f);
 	Rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+	Revolution= glm::vec3(0.0f, 0.0f, 0.0f);
 	Scale = glm::vec3(1.0f, 1.0f, 1.0f);
 	Color = glm::vec3(1.0f, 1.0f, 1.0f);
 	result = glm::mat4(1.0f);
 };//생성자
+
 Map::Map(glm::vec3 Position,
 	glm::vec3 Rotation,
+	glm::vec3 Revolution,
 	glm::vec3 Scale,
-	glm::vec3 Color) {
-	Position = Position;
-	Rotation = Rotation;
-	Scale = Scale;
-	Color = Color;
+	glm::vec3 Color) :Position{ Position }, Rotation{ Rotation }, Revolution{ Revolution }, Scale{ Scale }, Color{ Color } {
 };//기본값 생성
+
 Map::Map(const Map& object) {//복사 생성자
 
 	Translate = object.Translate;
@@ -22,52 +22,55 @@ Map::Map(const Map& object) {//복사 생성자
 	Rotation = object.Rotation;
 	Scale = object.Scale;
 	Color = object.Color;
-};//생성자
+};
 
-glm::mat4 Map:: Getmatrix() {//아직 수정 필요
+Map::~Map()
+{
+
+};
+
+glm::mat4 Map:: Getmatrix() {
 	result = glm::mat4(1.0f);
 	if (Parent)
 		result = Parent->Getmatrix();
-	result = glm::translate(result, Position);
-	result = glm::rotate(result, glm::radians(Rotation.x), glm::vec3(1.0, 0, 0));
-	result = glm::rotate(result, glm::radians(Rotation.y), glm::vec3(0, 1.0, 0));
-	result = glm::rotate(result, glm::radians(Rotation.z), glm::vec3(0, 0, 1.0));
+	result = glm::rotate(result, glm::radians(Revolution.x), glm::vec3(1.0, 0, 0));//공전
+	result = glm::rotate(result, glm::radians(Revolution.y), glm::vec3(0, 1.0, 0));//공전
+	result = glm::rotate(result, glm::radians(Revolution.z), glm::vec3(0, 0, 1.0));//공전
+	result = glm::translate(result, Position);//이동
+	result = glm::rotate(result, glm::radians(Rotation.x), glm::vec3(1.0, 0, 0));//자전
+	result = glm::rotate(result, glm::radians(Rotation.y), glm::vec3(0, 1.0, 0));//자전
+	result = glm::rotate(result, glm::radians(Rotation.z), glm::vec3(0, 0, 1.0));//자전
 	result = glm::scale(result, Scale);
 	return result;
-};
-void Map::Revolution(float x, float y, float z) {//공전 
-	glm::mat4 Rx = glm::mat4(1.0f);//x축 자전
-	glm::mat4 Ry = glm::mat4(1.0f);//y축 자전
-	glm::mat4 Rz = glm::mat4(1.0f);//z축 자전
-	result = glm::rotate(Rx, glm::radians(x), glm::vec3(x, 0.0, 0.0));//x축공전
-	result = glm::rotate(Ry, glm::radians(y), glm::vec3(0.0, y, 0.0));//y축공전
-	result = glm::rotate(Ry, glm::radians(z), glm::vec3(0.0, 0.0, z));//z축공전
-	result = Rx * Ry * Rz;
-
 };
 void Map:: Change_Positon(float x, float y, float z) {
 	Position = glm::vec3(x, y, z);
 };
+
 void Map:: Change_Rotation(float x, float y, float z) {
 	Rotation = glm::vec3(x, y, z);
+}
+void Map::Change_Revoltion(float x, float y, float z){
+	Revolution = glm::vec3(x, y, z);
 };
+
 void Map:: Change_Scale(float x, float y, float z) {
 	Scale = glm::vec3(x, y, z);
 };
+
 glm::vec3 Map:: Change_Color(float x, float y, float z) {
 	Color = glm::vec3(x, y, z);
 	return Color;
 };
-void Map:: Change_Translate(float x, float y, float z) {
-	Translate = glm::vec3(x, y, z);
-	result = glm::translate(result, Translate);
-};
-void Map:: Apply_Parent(Map* Parent1) {
+
+void Map::Apply_Parent(Map* Parent1) {
 	Parent = Parent1;
-}
-float Map:: Return_PositionX() {
+};
+
+float Map::Return_PositionX() {
 	return Position.x;
-}
-float Map:: Return_PositionZ() {
+};
+
+float Map::Return_PositionZ() {
 	return Position.z;
-}
+};
--- FPSGame/Map.h
+++ FPSGame/Map.h
@@ -10,6 +10,7 @@ private:
 	glm::vec3 Position{ 0 };//위치
 	glm::vec3 Translate{ 0 };//위치
 	glm::vec3 Rotation{ 0 };//회전값
+	glm::vec3 Revolution{ 0 };//공전값
 	glm::vec3 Scale{ 1 };//스케일
 	glm::vec3 Color{ 1 };//색깔
 	glm::mat4 result = glm::mat4(1.0f);
@@ -18,16 +19,17 @@ public:
 	Map();
 	Map(glm::vec3 Position,
 		glm::vec3 Rotation,
+		glm::vec3 Revolution,
 		glm::vec3 Scale,
 		glm::vec3 Color);
 	Map(const Map& object);
+	~Map();
 	glm::mat4 Getmatrix();
-	void Revolution(float x, float y, float z);
 	void Change_Positon(float x, float y, float z);
 	void Change_Rotation(float x, float y, float z);
+	void Change_Revoltion(float x, float y, float z);
 	void Change_Scale(float x, float y, float z);
 	glm::vec3 Change_Color(float x, float y, float z);
-	void Change_Translate(float x, float y, float z);
 	void Apply_Parent(Map* Parent1);
 	float Return_PositionX();
 	float Return_PositionZ();
