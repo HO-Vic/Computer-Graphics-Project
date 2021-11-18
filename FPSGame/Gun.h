6c7d9dd42780c185026e72e785fc361aed9f1cc4
PSH <shp9826@naver.com>
2021-11-18 15:57:54 +0900

    gun - pistol 자식 클래스 생성, gun 내용 추가

--- FPSGame/Gun.h
+++ FPSGame/Gun.h
@@ -1,5 +1,19 @@
 #pragma once
+#include<gl/glew.h>
+#include<gl/freeglut.h>
+#include<glm/glm/glm.hpp>
+#include<glm/glm/ext.hpp>
+#include<glm/glm/gtc/matrix_transform.hpp>
 class Gun
 {
+	glm::vec3 pos = glm::vec3(0, 0, 0);
+	float rotateAngleX = 0.0f;
+	float rotateAngleY = 0.0f;
+	float rotateAngleZ = 0.0f;
+	bool isAttack = false;
+public://virtual func
+	virtual void Attack();
+	virtual void reroad();
+	virtual void playerRun();
 };
 
